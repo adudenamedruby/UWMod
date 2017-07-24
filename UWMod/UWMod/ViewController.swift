71c380f7a3909c249d967828ff8442b30a86f824
roux george buciu <roux@acmelabs.ca>
2017-07-24 19:38:28 -0400

    Initial commit

---
+++ UWMod/UWMod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UWMod
+//
+//  Created by roux g. buciu on 2017-07-24.
+//  Copyright © 2017 roux g. buciu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
