818a2a5165a24281788d872082e051bacac066fc
roux george buciu <roux@acmelabs.ca>
2017-07-29 17:26:46 -0400

    Add mainCard and buttons

--- UWMod/UWMod/RoleSelectVC.swift
+++ UWMod/UWMod/RoleSelectVC.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class RoleSelectVC: UIViewController {
     
+    @IBOutlet weak var mainCardView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.mainCardView.layer.cornerRadius = 10
+
     }
     
     override func didReceiveMemoryWarning() {
