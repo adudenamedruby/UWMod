23bb833804e49f43b8af73debc0ca5e9c96ced49
roux george buciu <roux@acmelabs.ca>
2017-08-02 17:07:30 -0400

    Add roleExplanation to role cards

---
+++ UWMod/UWMod/File.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  UWMod
+//
+//  Created by roux g. buciu on 2017-08-02.
+//  Copyright © 2017 roux g. buciu. All rights reserved.
+//
+
+import Foundation
+
+class BasicAlertObject {
+    let title: String
+    let text: String
+    let buttonName: String
+    let additionalInformation: String
+    
+    init(title: String, text: String, buttonName: String = "", additionalInformation: String = "") {
+        self.title = title
+        self.text = text
+        self.buttonName = buttonName
+        self.additionalInformation = additionalInformation
+    }
+}
--- UWMod/UWMod/RoleExplanationVC.swift
+++ UWMod/UWMod/RoleExplanationVC.swift
@@ -9,11 +9,35 @@
 import UIKit
 
 class RoleExplanationVC: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var mainCardView: UIView!
+    @IBOutlet weak var headerCardView: UIView!
+    @IBOutlet weak var firstLetterLabel: OldTextRedLabel!
+    @IBOutlet weak var remainingTitleLabel: OldTextTanLabel!
+    @IBOutlet weak var backButton: PMSuperButton!
+    @IBOutlet weak var scrollTextView: UITextView!
+    
+    
+    // MARK: - Variables
+    
+    var alertInfo: BasicAlertObject!
+    
+    
+    // MARK: - View lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mainCardView.layer.cornerRadius = STYLE.CornerRadius
+        mainCardView.backgroundColor = STYLE.Tan
+        headerCardView.backgroundColor = STYLE.Brown
+        
+        firstLetterLabel.text = alertInfo.title.firstLetter()
+        remainingTitleLabel.text = alertInfo.title.restOfString()
+        scrollTextView.text = alertInfo.text
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +45,8 @@ class RoleExplanationVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismissButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
 
 }
